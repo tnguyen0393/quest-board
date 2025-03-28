3e04e26dcb0f44dc87942faf4d9187e0457702ff
Tien <tnguyen0393@gmail.com>
2025-03-28 12:31:29 -0500

    pages update

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module '*.css' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.svg' {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>
+  export default content
+}
